932d2ade7e954ee7f16178f86788a4beae101bc8
kangnwh <hnzz_kn@163.com>
2016-07-02 19:37:44 +0800

    using relative default report template folder

--- app/subapps/reporting/routing.py
+++ app/subapps/reporting/routing.py
@@ -2,12 +2,14 @@
 from flask import Blueprint
 from .common import render_html
 from BI.Reports import render_report
+import os
 
 reportRoute = Blueprint('reportRoute', __name__,
                      template_folder='templates', static_folder='static')
 
+REPORT_TEMPLATE_FOLDER = os.path.split(os.path.realpath(__file__))[0] + os.sep + "templates" +os.sep + "report"
 
 @reportRoute.route('/', methods=['GET', 'POST'])
 def index():
-    report = render_report('F:\\business\\MyECharts\\app\\subapps\\reporting\\templates\\report\\demo.xml',stock_code="000001")
+    report = render_report(REPORT_TEMPLATE_FOLDER+os.sep+'demo.xml',stock_code="000001")
     return render_html(report)
